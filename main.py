5d312c187822f3c9a32d4bb344de9e3a559cf7c0
Hila <hilapik12@gmail.com>
2022-03-02 23:55:36 +0200

    sending discover and request messages

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from time import sleep
 
 UDP_IP = "172.16.16.182"
 UDP_PORT = 2023
-DISCOVER_MESSAGE = b"discover"
+#DISCOVER_MESSAGE = b"discover"
+DISCOVER_MESSAGE="discover"
+OFFER_MESSAGE="offer"
+REQUEST_MESSAGE="request"
+ACKNOWLEDGE_MESSSAGE="acknowledge"
+
 
 print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
@@ -18,7 +23,21 @@ sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 while True:
 
-        sock.sendto(bytes("discover", "utf-8"), ("255.255.255.255", 2023))
-        print("sent")
-        data, addr = sock.recvfrom(1024)
+        sock.sendto(bytes(DISCOVER_MESSAGE, "utf-8"), ("255.255.255.255", 2023))
+        print("sent discover")
+        data, addr = sock.recvfrom(1024)  # expecting to recieve an offer msg
         sleep(1)
+        if data.startswith() == OFFER_MESSAGE:
+                msg=data.split(" ")  #["offer",ip adr]
+                ip_adr=msg[1]
+                REQUEST_MESSAGE=REQUEST_MESSAGE+" "+ip_adr
+                sock.sendto(bytes(REQUEST_MESSAGE, "utf-8"), ("255.255.255.255", 2023))
+                print("sent request")
+                data, addr = sock.recvfrom(1024)  # expecting to recieve an acknowledge msg
+                sleep(1)
+                if data == ACKNOWLEDGE_MESSSAGE:
+                        print("it works")
+                else:
+                        print("error")
+        else:
+                print("error")
