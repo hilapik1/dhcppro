aacce748892a02e84c5d26111ddfb30acfc7f2b2
cyber <hilapik12@gmail.com>
2022-03-15 10:54:04 +0200

    update the static mask part

--- file.py
+++ file.py
@@ -2,5 +2,10 @@ class Constants:
     src_port=2025
     dest_port=2023
 
+    MASK_PART = 0
+    IP_PART = 1
+    STATIC_MASK_PART = "255"
+    IP_SAPARATOR ="."
+
     def __init__(self):
         #NONE
