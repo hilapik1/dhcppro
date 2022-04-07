51709ba68ed3a6d469ac8e6753ce752cddcc58e3
hilapik1 <hilapik12@gmail.com>
2022-04-07 14:52:33 +0300

    update

--- client1.py
+++ client1.py
@@ -7,6 +7,7 @@ from scapy.all import Ether, IP, UDP, BOOTP, DHCP, sendp
 import random
 import os
 from file import Constants
+from threading import Thread
 
 os.system("python file.py")
 #import constant
@@ -35,66 +36,118 @@ def mac_to_bytes(mac_addr: str) -> bytes:
     return int(mac_addr.replace(":", ""), 16).to_bytes(6, "big")
 
 
-def filter(packet):
-    if UDP in packet:
-        if packet[UDP].dport == 2024:
-            return True
-    return False
-#srp1
+class DHCPHandler:
+
+    #def _init_(self, client_mac):
+    def _init_(self):
+        pass
+        #self.generate = DHCP_generator(Constants.src_port, Constants.dest_port, client_mac)
+
+    def filter(self, pack):
+        if not (DHCP in pack):
+            return False
+
+        if not(BOOTP in pack):
+            return False
+
+        if not(pack[BOOTP].xid in dict1.keys()):
+            return False
+
+        pa_in_dict = dict1[pack[BOOTP].xid]
+        if pack[BOOTP][DHCP].options[0][1] == Constants.OFFER:
+            if pa_in_dict[BOOTP][DHCP].options[0][1] == Constants.DISCOVER:
+                return True
+        elif pack[BOOTP][DHCP].options == Constants.ACK:
+            if pa_in_dict[BOOTP][DHCP].options == Constants.REQUEST:
+                return True
+
+        return False
+
+    def handle(self, pack):
+        #צריכה לעשות מחלקה נפרדת בקובץ נפרד של כל הטיפול בבקשות השונות (כמו שעשיתי בסרבר)
+        message_type = pack[BOOTP][DHCP].options[0][1]
+        if message_type == Constants.OFFER:
+            #handle_offer(pa)
+            #self.generate.request_generate(pack[BOOTP].siaddr, pack[BOOTP].yiaddr, pack[BOOTP].chaddr)
+            pa_in_dict = dict1[pack[BOOTP].xid]
+            request = request_generate(pack[BOOTP].siaddr, pack[BOOTP].yiaddr, pa_in_dict[BOOTP].chaddr)
+            sendp(request)
+
+        elif message_type == Constants.ACK:
+            #handle_ack()
+            print("ACK WAS RECIEVED")
+
+
+'''
 class DHCP_generator:
 
-    def __init__(self, src_port, dest_port, client_mac):
+    def _init_(self, src_port, dest_port, client_mac):
         self.src_port = src_port
         self.dest_port = dest_port
         self.mac = client_mac
+'''
+def discover_generate(mac):
+    #src_port=2025, dst_port=2023
+    dhcp_discover = (
+            Ether(dst="ff:ff:ff:ff:ff:ff") /
+            IP(src="0.0.0.0", dst="255.255.255.255") /
+            UDP(sport=Constants.src_port, dport=Constants.dest_port) /
+            BOOTP(
+                chaddr=mac_to_bytes(mac),
+                xid=777 #random.randint(1, 2 ** 32 - 1),
+            ) /
+            DHCP(options=[("message-type", "discover"), "end"])
+    )
+    dhcp_discover.show()
+    return dhcp_discover
 
-    def discover_generate(self):
-        #src_port=2025, dst_port=2023
-        self.dhcp_discover = (
-                Ether(dst="ff:ff:ff:ff:ff:ff") /
-                IP(src="0.0.0.0", dst="255.255.255.255") /
-                UDP(sport=self.src_port, dport=self.dest_port) /
-                BOOTP(
-                    chaddr=mac_to_bytes(self.mac),
-                    xid=776 #random.randint(1, 2 ** 32 - 1),
-                ) /
-                DHCP(options=[("message-type", "discover"), "end"])
-        )
-        self.dhcp_discover.show()
-        return self.dhcp_discover
+def request_generate(server_ip, client_ip, client_mac):
+    #src_port=2025, dst_port=2023
+    dhcp_request = (
+            Ether(dst="ff:ff:ff:ff:ff:ff") /
+            IP(src="0.0.0.0", dst="255.255.255.255") /
+            UDP(sport=Constants.src_port, dport=Constants.dest_port) /
+            BOOTP(chaddr=client_mac)/#mac_to_bytes(client_mac)) /
+            DHCP(options=[("message-type", "request"), ("server_id", server_ip),
+                          ("requested_addr", client_ip), "end"]))
+    return dhcp_request
 
-    def request_generate(self):
-        #src_port=2025, dst_port=2023
-        self.dhcp_request = (
-                Ether(dst="ff:ff:ff:ff:ff:ff") /
-                IP(src="0.0.0.0", dst="255.255.255.255") /
-                UDP(sport=Constants.src_port, dport=Constants.dest_port) /
-                BOOTP(chaddr=mac_to_bytes(client_mac)) /
-                DHCP(options=[("message-type", "request"), ("server_id", SETTINGS["serverIP"]),
-                              ("requested_addr", SETTINGS["clientIP"]), "end"]))
-        return self.dhcp_request
 
+def sniffer(filter, handler):
+    pa = sniff(lfilter=filter, prn=handler)
+
+
+handler = DHCPHandler()
+t = Thread(target=sniffer, args=(handler.filter, handler.handle))
+t.start()
+time.sleep(0.5)
 while True:
 
         client_mac = "18:60:24:8F:64:90"#"01:02:03:04:05:06"
-        generator = DHCP_generator(Constants.src_port, Constants.dest_port, client_mac)# src_port=2025, dest_port=2023
-        dhcp_discover = generator.discover_generate()
-        # SETTINGS = {"serverIP": dhcp_discover[BOOTP].siaddr, "clientIP": dhcp_discover[BOOTP].yiaddr, "XID": dhcp_discover[BOOTP].xid}
-        result = srp1(dhcp_discover, verbose=False)#, iface="Software Loopback Interface 1"  # expecting to recieve an offer msg
-        result.show()
-        print("hi")
-        SETTINGS = {"serverIP": dhcp_discover[BOOTP].siaddr, "clientIP": result[BOOTP].yiaddr,
-                    "XID": dhcp_discover[BOOTP].xid}
-        for packet in result:
+        #generator = DHCP_generator(Constants.src_port, Constants.dest_port, client_mac)# src_port=2025, dest_port=2023
+        #dhcp_discover = generator.discover_generate()
+        dhcp_discover = discover_generate(client_mac)
+        id = dhcp_discover[BOOTP].xid
+        dict1 = {}
+        dict1[id] = dhcp_discover
+        dhcp_discover.show()
+        sendp(dhcp_discover)
+        t.join()
+        #pa = sniff(lfilter=handler.filter, prn=handler.handle)
+        #SETTINGS = {"serverIP": dhcp_discover[BOOTP].siaddr, "clientIP": pa[BOOTP].yiaddr,
+        #            "XID": dhcp_discover[BOOTP].xid}
+        #result = srp1(dhcp_discover, verbose=False)#, iface="Software Loopback Interface 1"  # expecting to recieve an offer msg
+        #result.show()
+
+
+        #for packet in pa:
             #if DHCP in result and result[DHCP].options[0][1] == 2:
-            if DHCP in result and result[BOOTP][DHCP].options == 2:  # message type=2, that means offer message
-                    dhcp_request = generator.request_generate()
-                    print("sent request")
-                    result1 = srp1(dhcp_request, lfilter=filter)# expecting to recieve an acknowledge msg
-                    for packet in result1:
-                            if result1[BOOTP][DHCP].options == 5:
-                                    print("it works")
-                            else:
-                                    print("error")
-
-
+        #    if DHCP in pa and pa[BOOTP][DHCP].options == 2:  # message type=2, that means offer message
+        #            dhcp_request = generator.request_generate()
+        #            print("sent request")
+        #            result1 = srp1(dhcp_request, lfilter=filter)# expecting to recieve an acknowledge msg
+        #            for packet in result1:
+        #                    if result1[BOOTP][DHCP].options == 5:
+        #                            print("it works")
+        #                   else:
+        #                            print("error")
